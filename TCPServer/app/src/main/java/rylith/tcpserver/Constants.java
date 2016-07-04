d40ee7c1ff59c88f1dd0a6747294a1dd491fd843
Rylith <ogaoni@gmail.com>
2016-07-04 15:33:54 +0200

    Init push

---
+++ TCPServer/app/src/main/java/rylith/tcpserver/Constants.java
@@ -0,0 +1,11 @@
+package rylith.tcpserver;
+
+/**
+ * Created by antoi on 23/06/2016.
+ */
+public class Constants {
+
+    public static final String CLOSED_CONNECTION = "kazy_closed_connection";
+    public static final String LOGIN_NAME = "kazy_login_name";
+
+}
